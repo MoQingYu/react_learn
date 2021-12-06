efc1c620e65d270fb898d99daca6c8de72ae5cf7
刘旭雷 <esperanza_liu@163.com>
2021-12-06 09:30:58 +0800

    update

--- src/unit.js
+++ src/unit.js
@@ -27,23 +27,23 @@ class NativeUnit extends Unit {
     let tagEnd = `</${type}>`;
     let childrenString = '';
     this._renderedChildrenUnit = []
-    for (let propsName in props) {
-      if(propsName === 'className') { // 处理类名
-        tagStart += `class="${props[propsName]}" `
-      } else if(propsName === 'style') { // 处理样式
+    for (let propName in props) {
+      if(propName === 'className') { // 处理类名
+        tagStart += `class="${props[propName]}" `
+      } else if(propName === 'style') { // 处理样式
         tagStart += `style="${
           Object
-            .entries(props[propsName])
+            .entries(props[propName])
             .map(([key, value])=> {
               return `${key.replace(/[A-Z]/, m=> `-${m.toLowerCase()}`)}: ${value}`
             })
             .join(";")
         }" `
-      } else if(/^on[A-Z]/.test(propsName)) { // 绑定事件
-        const eventName = propsName.slice(2).toLowerCase()
-        $(document).delegate([`data-reactid=${reactId}`], `${eventName}.${this._reactId}`, props[propsName]);
-      } else if(propsName === 'children') {
-        (props[propsName]).forEach((child, index) => {
+      } else if(/^on[A-Z]/.test(propName)) { // 绑定事件
+        const eventName = propName.slice(2).toLowerCase()
+        $(document).delegate([`data-reactid=${reactId}`], `${eventName}.${this._reactId}`, props[propName]);
+      } else if(propName === 'children') {
+        (props[propName]).forEach((child, index) => {
           const childUnit = createUnit(child); 
           childUnit._mountIndex = index; // 记录当前子节点在所有子节点中的位置
           this._renderedChildrenUnit.push(childUnit);
@@ -51,27 +51,69 @@ class NativeUnit extends Unit {
           childrenString += childMarkup;
         });
       } else {
-        tagStart += `${propsName}=${props[propsName]} `;
+        tagStart += `${propName}=${props[propName]} `;
       }
     }
     return `${tagStart}>${childrenString}${tagEnd}`;
   }
 
+  update(nextElement) {
+    const oldProps = this._currentElement.props;
+    const newProps = nextElement.props;
+    this.updateDOMProperties(oldProps, newProps);
+  }
+
+  updateDOMProperties(oldProps, newProps) {
+    let propName;
+    for(propName in oldProps) {
+      if(!newProps.hasOwnProperty(propName)) {
+        $(`[data-reactid=${this._reactId}]`).removeAttr(propName);
+      }
+      if(/^on[A-Z]/.test(propName)) {
+        $(document).undelegate(`.${this._reactId}`);
+      }
+    }
+    for(propName in newProps) {
+      if(props === "children") { //如果是children属性，就跳过，暂不处理
+        continue;
+      } else if(/^on[A-Z]/.test(propName)) {
+        const eventName = propName.slice(2).toLowerCase(); //获取事件名称
+        $(document).delegate(`[data-reactid]=${this._reactId}`, `${eventName}.${this._reactId}`, newProps[propName]);
+      } else if(propName === 'className') {//将新的类名赋值给当前的元素
+        $(`[data-reactid=${this._reactId}]`).attr('class', newProps[propName]);
+      } else if(propName === 'style') {
+        const styleObj = newProps[propName];
+        Object.entries(styleObj).map(([attr, value]) => {
+          $(`[data-reactid=${this._reactId}]`).css(attr, value);
+        })
+      } else {
+        $(`[data-reactid=${this._reactId}]`).props(propName, newProps[propName]);
+      }
+    }
+  }
+
 }
 
 class CompositeUnit extends Unit {
-
+  // 组件更新操作
   update(nextElement, partialState) {
-    this._currentElement = nextElement || this._currentElement;
+    // 获取到新的元素
+    this._currentElement = nextElement || this._currentElement
+    // 获取新的状态，不管要不要更新组件，组件的状态一定要修改
     const nextState = Object.assign(this._componentInstance.state, partialState);
+    // 获取新的属性对象
     const nextProps = this._currentElement.props;
     if(this.componentInstance.shouldComponentUpdate && !this.componentInstance.shouldComponentUpdate(nextProps, nextState)) {
       return ;
     }
+    // 获取上次渲染的单元
     const preRenderedUnitInstance = this._renderedUnitInstance;
+    // 得到上次渲染的元素
     const preRenderedElement = preRenderedUnitInstance.renderedElement;
     const nextRenderedElement = this._componentInstance.render();
+    // 如果新旧两个元素类型一样,则可以进行深度的比较,如果币一样,就直接干掉老的元素,重新创建
     if(shouldDeepCompare(preRenderedElement, nextRenderedElement)) {
+      // 如果可以进行审比较,则把更新的工作教给上次渲染出来的那个element元素对应的unit进行处理
       preRenderedUnitInstance.update(nextRenderedElement);
       this._componentInstance.componentDidUpdate && this._componentInstance.componentDidUpdate()
     } else {
